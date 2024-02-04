6acbe4ae30a0e163db7a3b15656651b64b2acf2f
KamysDev <sereginka070504@gmail.com>
2024-02-04 23:44:18 +0700

    Maybe completed lab1(?)

--- lab 1/Program.cs
+++ lab 1/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using ConsoleApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
-namespace ConsoleApp1
+namespace ConsoleApp
 {
     class Account
     {
@@ -17,21 +16,19 @@ namespace ConsoleApp1
         {
             _balance = 0;
             _notifyer = [];
-
         }
 
         public Account(decimal balance)
         {
             _balance = balance;
             _notifyer = [];
-
         }
 
         public void AddNotifyer(INotifyer notifyer)
         {
             _notifyer.Add(notifyer);
         }
-
+        
         public void ChangeBalance(decimal value)
         {
             _balance += value;
@@ -70,7 +67,7 @@ namespace ConsoleApp1
 
         public void Notify(decimal _balance)
         {
-            if (_balance < _lowBalanceValue)
+            if (_balance <= _lowBalanceValue)
             {
                 Console.WriteLine("SMSLowBalanceNotifyer");
                 Console.WriteLine(_phone);
@@ -92,7 +89,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("EMailBalanceChangedNotifyer");
             Console.WriteLine(_email);
-            Console.WriteLine($"Алёрт! Стипендия пришла! Текущий баланс {_balance}\n");
+            Console.WriteLine($"Алёрт! Баланс поменялся! Текущий баланс {_balance}\n");
         }
     }
 
@@ -100,15 +97,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Пример без использования конструктора с полями\n");
+            
+            // Создаём аккаунт и базовые уведомления
             Account account = new Account();
             EMailBalanceChangedNotifyer emailNotification = new EMailBalanceChangedNotifyer("work.kamysdev@gmail.com");
             SMSLowBalanceNotifyer smsNotification = new SMSLowBalanceNotifyer("+79832516744", 999);
 
+            // Отправляем уведомления в лист, меняем баланс
             account.AddNotifyer(emailNotification);
             account.AddNotifyer(smsNotification);
-            account.ChangeBalance(1000);
-            account.ChangeBalance(1200);
+            account.ChangeBalance(15000);
+            account.ChangeBalance(-10000);
+            account.ChangeBalance(-4599);
 
+            Console.WriteLine("\nПример с использованием конструктора с полями\n");
+
+            // Всё то же самое, что и выше
+            Account account1 = new Account(1500);
+            account1.AddNotifyer(emailNotification);
+            account1.AddNotifyer(smsNotification);
+            account1.ChangeBalance(4300);
+            account1.ChangeBalance(-1400);
+            account1.ChangeBalance(-5000);
         }
     }
 }
