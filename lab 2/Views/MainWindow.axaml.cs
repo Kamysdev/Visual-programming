ce80d44a0ec322e11e7e8ce7c65e11d037807240
KamysDev <sereginka070504@gmail.com>
2024-02-08 19:29:44 +0700

    Modified

--- lab 2/Views/MainWindow.axaml.cs
+++ lab 2/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Media;
+using ReactiveUI;
 
 namespace lab_2.Views;
 
@@ -11,53 +13,11 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    public void ClickKhaki(object sender, RoutedEventArgs args)
+    public void ClickButton(object sender, RoutedEventArgs args)
     {
-        squarePan.Background = new SolidColorBrush(Colors.Khaki);
-    }
-
-    public void ClickRed(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.Red);
-    }
-    
-    public void ClickMediumVioletRed(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.MediumVioletRed);
-    }
-    
-    public void ClickBisque(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.Bisque);
-    }
-    
-    public void ClickLemonChiffon(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.LemonChiffon);
-    }
-    
-    public void ClickPowderBlue(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.PowderBlue);
-    }
-    
-    void ClickMintCream(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.MintCream);
-    }
-    
-    void ClickMaroon(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.Maroon);
-    }
-    
-    void ClickRosyBrown(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.RosyBrown);
-    }
-    
-    void ClickLightPink(object sender, RoutedEventArgs args)
-    {
-        squarePan.Background = new SolidColorBrush(Colors.LightPink);
+        if(sender is Button button && squarePan != null)
+        {
+            squarePan.Background = button.Background;
+        }
     }
 }
