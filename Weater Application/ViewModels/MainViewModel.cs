c9201e82e1c0f545b7dc14467d8d3fc575a137ba
KamysDev <work.kamysdev@gmail.com>
2024-03-08 01:43:04 +0700

    Updated project to Avalonia 11.0.6

--- Weater Application/ViewModels/MainViewModel.cs
+++ Weater Application/ViewModels/MainViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace Weater_Application.ViewModels;
+﻿using System.Threading;
+using System.Text.Json;
 
-public class MainViewModel : ViewModelBase
+namespace Weater_Application.ViewModels;
+
+public class MainViewModel : BaseViewModel
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public string CurrentDay
+    {
+        get => _CurrentDay;
+        set { _CurrentDay = value; OnPropertyChanged(nameof(CurrentDay)); }
+    }
+
+    public string _CurrentDay { get; set; }
+
+    public string CurrentTemp { get; set; }
+
+
+    public MainViewModel()
+    {
+        SetCurrentTime();
+        CurrentTemp = "-27";
+    }
+
+    private void SetCurrentTime()
+    {
+        CurrentDay = System.DateTime.Now.DayOfWeek.ToString() + ", " + System.DateTime.Now.Day.ToString();
+    }
+
+    public void CreateAPICall()
+    {
+
+    }
 }
