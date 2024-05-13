c8bd302b19c3f3228d2f4cad395dee2cfbd2aed0
KamysDev <work.kamysdev@gmail.com>
2024-05-13 20:43:42 +0700

    Added android proj

---
+++ TEST BUILDS/Weather Android/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Weather_Android.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ TEST BUILDS/Weather Android/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Weather_Android.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
